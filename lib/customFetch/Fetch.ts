719b6b7fff0a1b3fc95a63cffd8fc2ddbffa8430
Soumesh Kundu <iamsoumo26@gmail.com>
2026-02-07 02:02:47 +0530

    feat: backend route

--- lib/customFetch/Fetch.ts
+++ lib/customFetch/Fetch.ts
@@ -1,10 +1,11 @@
 export function Fetch(url: string, options?: RequestInit) {
-    return fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api${url}`, {
+    return fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/backend/api${url}`, {
         ...options,
         headers: {
           "Content-Type": "application/json",
             ...options?.headers,
         },
+        mode: 'cors',
         credentials: 'include',
     });
 }
--- lib/customFetch/serverFetch.ts
+++ lib/customFetch/serverFetch.ts
@@ -4,7 +4,7 @@ export async function serverFetch(url: string, options?: RequestInit) {
     const cookieStore = await cookies();
     const cookieHeader = cookieStore.toString();
     
-    return fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api${url}`, {
+    return fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/backend/api${url}`, {
         ...options,
         headers: {
             ...options?.headers,
