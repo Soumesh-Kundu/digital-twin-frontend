edc0ca6b0e2d1c208a0c13b5f9dcde47ea2c63c2
Soumesh Kundu <iamsoumo26@gmail.com>
2026-02-07 03:25:47 +0530

    socket url updated

--- stores/socket.ts
+++ stores/socket.ts
@@ -35,7 +35,7 @@ export const useSocketStore = create<SocketState & SocketActions>((set, get) => 
       return;
     }
 
-    const newSocket = io(process.env.NEXT_PUBLIC_SERVER_URL as string, {
+    const newSocket = io(process.env.NEXT_PUBLIC_SERVER_URL as string + "/backend", {
       transports: ["websocket", "polling"],
       autoConnect: true,
       reconnection: true,
